33045ad56d70f75b35d42bf825dec02106231999
Thindex <thinudex100@gmail.com>
2017-03-01 10:57:57 -0700

    Implemented methods in ppm.h

--- src/PPM.h
+++ src/PPM.h
@@ -12,5 +12,6 @@ public:
     PPM(std::string name, std::string type, int width, int height, int max);
     void set_pixel(int x, int y, int color);
     void draw_line(int x1, int y1, int x2, int y2, int color);
+    FILE* save_ppm(std::string name);
 
 };
--- src/ppm.cpp
+++ src/ppm.cpp
@@ -23,7 +23,26 @@ void PPM :: set_pixel(int x, int y, int color) {
 }
 
 void PPM :: draw_line(int x1, int y1, int x2, int y2, int color) {
-    
+    bool flipped = false;
+
+    // determine whether or not the variables need to be flipped, to the slope
+    // equation doesn't get all borked up
+    if(x1 > x2) {
+        std::swap(x2, x1);
+        std::swap(y2, y1);
+        flipped = true;
+
+    }
+
+    double m = ((double) (y2 - y1)) / 
+               ((double) (x2 - x1));  // determine slope of the line being drawn
+    double b = y1 - (m * x1);         // determine line constant
+
+    for(double x = x1; x <= x2; x += 1) {
+        int yToBeDrawn = (m * x) + b;
+        set_pixel((int) x, yToBeDrawn);
+
+    }
     
 }
 
@@ -32,4 +51,24 @@ void PPM :: draw_triangle(int x1, int y1, int x2, int y2, int x3, int y3, int co
     draw_line(x1, y1, x3, y3, color);
     draw_line(x2, y2, x3, y3, color);
     
+}
+
+FILE* PPM :: save_ppm(std::string name) {
+    ofstream output;
+    output.open(name);
+    output << "P3\n" + this->width + ' ' + this->height + '\n' + this->max + '\n';
+    
+    for(int i = 0; i < width; i++) {
+        for(int j = 0; j < height; j++) {
+            int color = map[i][j];
+            int r = (color >> 4) & 0xFF;
+            int g = (color >> 2) & 0xFF;
+            int b = (color) & 0xFF;
+
+            output << r + ' ' + g + ' ' + b;
+
+        }
+
+    }
+
 }
