503fe0e5c3c58456e7867c47572f1530915dc215
Thindex <thinudex100@gmail.com>
2017-03-01 22:08:49 -0700

    full implementation of PPM class

--- src/PPM.h
+++ src/PPM.h
@@ -12,6 +12,7 @@ public:
     PPM(std::string name, std::string type, int width, int height, int max);
     void set_pixel(int x, int y, int color);
     void draw_line(int x1, int y1, int x2, int y2, int color);
-    FILE* save_ppm(std::string name);
+    void save_ppm();
+    void draw_triangle(int x1, int y1, int x2, int y2, int x3, int y3, int color);
 
 };
