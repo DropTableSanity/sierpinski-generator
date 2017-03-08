dfed1ab626431cc859e0cf8ce2cb0cf5aa39d662
Thindex <thinudex100@gmail.com>
2017-03-08 14:31:27 -0700

    Changed get_halfway_points to return a double array instead of a tuple

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -1,21 +1,29 @@
+#include "Triangle.h"
+
 using namespace std;
 
-Triangle::Triangle(int x1, int y1, int x2, int y2, int x3, int y3) { 
+Triangle :: Triangle(double x1, double y1, double x2, double y2, double x3, double y3) { 
     this->x1 = x1;
     this->x2 = x2;
     this->x3 = x3;
     this->y1 = y1;
     this->y2 = y2;
-    this->y3 = y3;i
+    this->y3 = y3;
+    this->color = color;
 
 }
 
 
-std::tuple<double, double, 
-           double, double, 
-           double, double> Triangle::get_halfvay_points() {
-    return std::make_tuple((x1 + x2) / 2, (y1 + y2) / 2,
-                           (x1 + x3) / 2, (y1 + y3) / 2,
-                           (x2 + x3) / 2, (y2 + y3) / 2)
+double * Triangle :: get_halfway_points() {
+    static double halfwayPoints[6] = {(x1 + x2) / 2, (y1 + y2) / 2,
+                               (x1 + x3) / 2, (y1 + y3) / 2,
+                               (x2 + x3) / 2, (y2 + y3) / 2};
+    return halfwayPoints;
+
+}
+
+
+double Triangle :: get_side_length() {
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 
 }
--- src/Triangle.h
+++ src/Triangle.h
@@ -1,15 +1,16 @@
 #include <tuple>
-#include "Triangle.cpp"
+#include <cmath>
 
 class Triangle {
 
 private:   
-    int x1, x2, x3;
-    int y1, y2, y3;
+    double x1, x2, x3;
+    double y1, y2, y3;
     int color;
 
 public:
-    Triangle(int x1, int y1, int x2, int y2, int x3, int y3);
-    std::tuple<int, int, int, int, int, int> get_halfway_points();    
+    Triangle(double x1, double y1, double x2, double y2, double x3, double y3);
+    double *  get_halfway_points();    
+    double get_side_length();
 
 };
